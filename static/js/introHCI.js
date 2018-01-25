2b3013621048aecf3fe93506d6029f07ada82fe6
Emily Nguyen <emn001@ucsd.edu>
2018-01-24 19:35:10 -0800

    Add active class and toggle project descriptions

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,14 +5,35 @@ $(document).ready(function() {
 	initializePage();
 })
 
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+    	$(".project-description").fadeToggle();
+    }
+}
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$(this).text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
+
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+
 }
